2f06120677402e42aa2679d88abe060508313051
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-12-12 16:34:33 +0100

    add: visible hotels list

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,5 +51,21 @@
 </head>
 <body>
     
+    
+
+            <?php foreach ($hotels as $curHotel) { ?>
+                    <article>
+                        <h2>
+                            <?php echo $curHotel['name']; ?>
+                        </h2>
+                        <p><?php echo $curHotel['description']; ?></p>
+                        <p><?php echo $curHotel['parking']; ?></p>
+                        <p><?php echo $curHotel['vote']; ?></p>
+                        <p><?php echo $curHotel['distance_to_center']; ?></p>
+                    </article>
+                <?php } ?>
+            <!-- </section> -->
+            <hr>
+
 </body>
 </html>
